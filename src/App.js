da28ef0325a63e90e820017a6ea970853fa8dc16
Roberto Jarquin <robertoj@nicasource.com>
2022-05-09 22:22:58 -0600

    nuevo juego

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import GameVocales from "./components/GameVocales";
+import GameNumeros from "./components/GameNumeros";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <GameVocales />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/juego/numeros" element={<GameNumeros />} />
+        <Route path="juego/vocales" element={<GameVocales />} />
+        <Route path="/" element={<GameVocales />} />
+      </Routes>
+    </div>
+  );
 }
--- src/components/GameNumeros.js
+++ src/components/GameNumeros.js
@@ -5,71 +5,59 @@ import VocalContainer from "./VocalContainer";
 import PuntajeContainer from "./PuntajeContainer";
 import { Link } from "react-router-dom";
 
-//https://postimg.cc/gallery/p9n8dxz
-
 const MiVocales = [
   {
-    vocal: "a",
-    img_line:
-      "https://i.postimg.cc/mgYYK8tm/Whats-App-Image-2022-04-16-at-12-11-06-AM-3.jpg",
+    vocal: "1",
     img:
-      "https://i.postimg.cc/V6VnXhVk/Whats-App-Image-2022-04-16-at-12-11-07-AM.jpg"
+      "https://i.postimg.cc/PxFxjTcy/Whats-App-Image-2022-05-08-at-7-54-27-PM.jpg"
   },
   {
-    vocal: "e",
-    img_line:
-      "https://i.postimg.cc/dQfjChbj/Whats-App-Image-2022-04-16-at-12-11-06-AM-2.jpg",
+    vocal: "2",
     img:
-      "https://i.postimg.cc/1zyGSw-Fs/Whats-App-Image-2022-04-16-at-12-11-07-AM-1.jpg"
+      "https://i.postimg.cc/SxWJM02Q/Whats-App-Image-2022-05-08-at-7-54-26-PM-3.jpg"
   },
   {
-    vocal: "i",
-    img_line:
-      "https://i.postimg.cc/cLy7DV4K/Whats-App-Image-2022-04-16-at-12-11-06-AM-1.jpg",
+    vocal: "3",
     img:
-      "https://i.postimg.cc/hjjVznMY/Whats-App-Image-2022-04-16-at-12-11-07-AM-2.jpg"
+      "https://i.postimg.cc/LXsXwnyh/Whats-App-Image-2022-05-08-at-7-54-26-PM-2.jpg"
   },
   {
-    vocal: "o",
-    img_line:
-      "https://i.postimg.cc/qMCXxk0V/Whats-App-Image-2022-04-16-at-12-11-06-AM.jpg",
+    vocal: "4",
     img:
-      "https://i.postimg.cc/mrVQgk3M/Whats-App-Image-2022-04-16-at-12-11-07-AM-3.jpg"
+      "https://i.postimg.cc/pdRyQ0zf/Whats-App-Image-2022-05-08-at-7-54-26-PM-1.jpg"
   },
   {
-    vocal: "u",
-    img_line:
-      "https://i.postimg.cc/kGLNPHQp/Whats-App-Image-2022-04-16-at-12-11-05-AM.jpg",
+    vocal: "5",
     img:
-      "https://i.postimg.cc/Vv0MgFxB/Whats-App-Image-2022-04-16-at-12-11-07-AM-4.jpg"
+      "https://i.postimg.cc/zf7vxnF9/Whats-App-Image-2022-05-08-at-7-54-26-PM.jpg"
   }
 ];
 
 const MiVocalesLine = [
   {
-    vocal: "a",
+    vocal: "1",
     img_line:
-      "https://i.postimg.cc/mgYYK8tm/Whats-App-Image-2022-04-16-at-12-11-06-AM-3.jpg",
+      "https://i.postimg.cc/VL0Sz0zC/Whats-App-Image-2022-05-08-at-7-54-25-PM-2.jpg",
   },
   {
-    vocal: "e",
+    vocal: "2",
     img_line:
-      "https://i.postimg.cc/dQfjChbj/Whats-App-Image-2022-04-16-at-12-11-06-AM-2.jpg",
+      "https://i.postimg.cc/tRMZ0NGb/Whats-App-Image-2022-05-08-at-7-54-25-PM-1.jpg",
   },
   {
-    vocal: "i",
+    vocal: "3",
     img_line:
-      "https://i.postimg.cc/cLy7DV4K/Whats-App-Image-2022-04-16-at-12-11-06-AM-1.jpg",
+      "https://i.postimg.cc/FHtYzFKn/Whats-App-Image-2022-05-08-at-7-54-25-PM.jpg",
   },
   {
-    vocal: "o",
+    vocal: "4",
     img_line:
-      "https://i.postimg.cc/qMCXxk0V/Whats-App-Image-2022-04-16-at-12-11-06-AM.jpg",
+      "https://i.postimg.cc/gjnxXnR1/Whats-App-Image-2022-05-08-at-7-54-24-PM-1.jpg",
   },
   {
-    vocal: "u",
+    vocal: "5",
     img_line:
-      "https://i.postimg.cc/kGLNPHQp/Whats-App-Image-2022-04-16-at-12-11-05-AM.jpg",
+      "https://i.postimg.cc/PJ9PmTQJ/Whats-App-Image-2022-05-08-at-7-54-24-PM.jpg",
   }
 ];
 
@@ -105,7 +93,7 @@ export default function GameVocales() {
   return (
     <div className="container">
       <div className="header">
-        <p>Coloca las vocales donde corresponda.  <Link to="/juego/numeros">(Cambiar juego)</Link></p>
+        <p>Coloca las numeros donde corresponda. <Link to="/juego/vocales">(Cambiar juego)</Link></p>
       </div>
       <div className="side">
         <div className="vocales">{vocales}</div>
